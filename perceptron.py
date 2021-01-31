953d8733f4c97601fd7e56829be24639dba66b1b
sipocz <sipoczlaszlo@gmail.com>
2021-01-31 04:24:12 +0100

    202101310424

--- perceptron.py
+++ perceptron.py
@@ -11,18 +11,28 @@ class perceptron:
             f.append(i)
         pass
 
+    def vek_add(self,v1,v2):
+        o=v1
+        for i in range(len(v2)):
+            o[i]+=v2[i]
+        print("vek_ADD",o)
+        return(o)        
+
+
     def vek_skalar_mul(self,v,s):
-        print("MUL:",v)
-        su=0
+        print("MUL:",v, "skalar:",s)
+        o=[]
         for i in range(len(v)):
-            su+=v[i]*s
-        return(su)
+            o.append(v[i]*s)
+        print("MUL out:",o)
+        
+        return(o)
 
     def vek_mul(self,v1,v2):
-        sum=0
+        su=0
         for i in range(len(v1)):
-            sum+=v1[i]*v2[i]
-        return(sum)
+            su+=v1[i]*v2[i]
+        return(su)
     
     def mat_mul(self,M,v):
         o=[]
@@ -30,6 +40,8 @@ class perceptron:
             o.append(M[i],v)
         return(o)
     
+    
+
     def convert_to_01(self,v):
         o=[]
         for i in v:
@@ -54,8 +66,9 @@ class perceptron:
         self.bias=0
 
         for self.akt in range(self.n_epochs):
-            Xakt=self.X[self.akt]
+            #Xakt=self.X[self.akt]
             print("Starting epoch:", self.akt)
+            print("Weights:",self.weights)
             for x,label in zip(X,y):
                 y_=label
                 # perceptron esetén [-1, 1] a tartomány
@@ -64,20 +77,28 @@ class perceptron:
                 
                 
                 if (self.vek_mul(self.weights,x)+self.bias)*y_<=0:
-                    self.weights+=self.vek_skalar_mul(x,y_*self.lr)
+                    print("Weigths korrection")
+                    self.weights=self.vek_add(self.weights,self.vek_skalar_mul(x,y_*self.lr))
+                    self.bias=self.bias+y_*self.lr
         self.fitted=True
         print("Finished training.")
     
     def predict(self,X):
         if not self.fitted:
             raise ValueError("Perceptron model is not fitted")
-        elif X.shape[1] != self.n_features:
+        elif len(X[0]) != self.n_features:
             raise ValueError(f"Incorrect number of input features (expected {self.n_features})")
         else:
             
             # Please think through and explain the trainer, WHY this is a matmul!?
             #You can remove the line, just here to make you think.
-            activations = self.mat_mul(X, self.weights) + self.bias
+            activations=[]
+            for i in range(len(X)):
+                activations.append(self.vek_mul(X[i],self.weights)+self.bias)
+
+            
+            
+            
             o = self.convert_to_01(activations) 
             # Tricky conversion of signs to 0/1 labels
             return o        
@@ -85,5 +106,6 @@ class perceptron:
 X=[[0,0],[0,1],[1,0],[1,1]]
 y=[0,0,0,1]
 pcn=perceptron()
-pcn.fit(X,y,n_epochs=100)
+pcn.fit(X,y,n_epochs=1000)
+print(pcn.predict(X))
 
